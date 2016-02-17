67559b90e6571d4e119d36938bfcdafa402eb424
Craig Mosey <craig.mosey@gmail.com>
2016-02-17 20:48:14 +0000

    started addWaiterInstruction function

--- assig 2/assig 2/httpDispacher.cpp
+++ assig 2/assig 2/httpDispacher.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #define WORKER_THREAD_COUNT 10
-
+#define MAX_QUEUE_SIZE 20
 
 httpDispacher::httpDispacher()
 {
@@ -14,18 +14,45 @@ httpDispacher::~httpDispacher()
 {
 	//Close all worker threads
 	closeThreads = true;
-	for (auto i = workerThreads.begin(); i != workerThreads.end(); i++) {
+	for (auto i = workerThreads.begin(); i != workerThreads.end(); i++) 
+	{
 		auto& tmp = *i;
-		
+		tmp.join();
+		//TODO: delete the thread
 	}
 }
 
 void httpDispacher::startThreads()
 {
 	//Populate array
-	for (int i = 0; i < WORKER_THREAD_COUNT; i++) {
+	for (int i = 0; i < WORKER_THREAD_COUNT; i++) 
+	{
 		//Add the thread to the vecotr
 		workerThreads.push_back(std::thread(&httpDispacher::workerThread));
+	}
+}
+
+void httpDispacher::addWaiterInstruction(SOCKET* clientSocket)
+{
+	dispacherMutex.lock();
+	if (queuedRequests.size() > MAX_QUEUE_SIZE)
+	{
+		dispacherMutex.unlock();
+		char* message = "Too many connections, try again later\n";//TODO: add http header
+		send(*clientSocket, message, strlen(message), 0);
+
+		//close connection
+		closesocket(*clientSocket);
+		
+		delete clientSocket;
+		return; 
+	}
+	else 
+	{
+		dispacherMutex.unlock();
+		waiterInsruction* tmp = new waiterInsruction();
+		tmp->clientSocket = clientSocket;
+
 	}
 }
 
--- assig 2/assig 2/httpDispacher.h
+++ assig 2/assig 2/httpDispacher.h
@@ -1,6 +1,20 @@
 #pragma once
 #include <thread>
 #include <vector>
+#include <stdio.h>
+#include <winsock2.h>
+#include <string>
+#pragma comment(lib,"ws2_32.lib") 
+
+enum instruction{ reply, noJob, close };
+
+struct waiterInsruction
+{
+	std::string* request;
+	SOCKET* clientSocket;
+	instruction inst;
+};
+
 
 class httpDispacher
 {
@@ -10,6 +24,9 @@ public:
 	void startThreads();
 private:
 	std::vector<std::thread> workerThreads;
+	std::vector<waiterInsruction> queuedRequests;
+	std::mutex dispacherMutex;
+	void addWaiterInstruction(SOCKET* clientSocket);
 	void workerThread();
 	bool closeThreads = false;
 };
